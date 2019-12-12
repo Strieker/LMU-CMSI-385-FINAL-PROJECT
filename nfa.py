c57acfae380266e4298be8cf2c826616f3651b7b
Sage Strieker <sspink157@gmail.com>
2019-12-11 21:21:13 -0800

    made lambdas work

--- nfa.py
+++ nfa.py
@@ -1,7 +1,7 @@
 import sys
 from nfa_stdin import NFAStdin
 LAMBDA = ""
-# WEIRD CASE WHERE YOU GO TO ANOTHER VALUE AND THE STRING THINGS ACCEPTED WHEN IT'S NOT 
+#CHECK YOU DONT GET ACCEPTED WHEN AT ACCEPT STATE BUT STILL A STRING LEFT 
 class State:
     value = None
     transitions = []
@@ -23,9 +23,14 @@ class NFAProblem:
         current_states_to_check_current_transitions_on = []
         graveyard = []
         current_states_to_check_current_transitions_on.append(current_state)
+        graveyard.append(current_state)
         lambdas = []
         lambda_transitions = []
         while len(current_states_to_check_current_transitions_on) != 0:
+            current_states_to_check_current_transitions_on = [] if len(current_states_to_check_current_transitions_on) == 1 else current_states_to_check_current_transitions_on[1::]   
+            print("current state value: " + str(current_state.value))
+            print("to expand: ")
+            print([x.value for x in current_states_to_check_current_transitions_on])
             if len(possible_accepted_string) == 0 and current_state.accepted:
                     return True
             else:
@@ -34,21 +39,30 @@ class NFAProblem:
                     if len(possible_accepted_string) > 0:
                         if possible_accepted_string[0] in x:
                             filtered_transitions.append(x)
-                        elif LAMBDA in x:
+                        if LAMBDA in x:
                             filtered_transitions.append(x)
-                            lambda_transitions.append(x)
+                            lambda_transitions.append(x) 
                 lambdas = [y for x in lambda_transitions for y in x if y == x[1]]
+                print("lambdas: ")
+                print(lambdas)
                 filtered_states = [y for x in filtered_transitions for y in x if y == x[1]]
+                # print("filtered states: ")
+                # print(filtered_states)
                 if len(filtered_states) > 0:
                     for state in filtered_states:
-                        if state not in current_states_to_check_current_transitions_on and state not in graveyard:
+                        if state in graveyard:
+                            continue
+                        else:
                             current_states_to_check_current_transitions_on.append(self.find_state_in_machine(state))
-                state_to_pass = current_states_to_check_current_transitions_on[0]  
-                current_states_to_check_current_transitions_on = current_states_to_check_current_transitions_on[1::]
-                graveyard.append(state_to_pass)
-                current_state = state_to_pass
-            if(current_state.value not in lambdas):
-                possible_accepted_string = "" if len(possible_accepted_string) <= 1 else possible_accepted_string[1::]
+                if len(current_states_to_check_current_transitions_on) > 0:
+                    state_to_pass = current_states_to_check_current_transitions_on[0]  
+                    graveyard.append(state_to_pass)
+                    current_state = state_to_pass
+                    if current_state.value not in lambdas:
+                        print("made it")
+                        possible_accepted_string = "" if len(possible_accepted_string) <= 1 else possible_accepted_string[1::]
+                        print(possible_accepted_string)
+            
         if len(possible_accepted_string) == 0 and current_state.accepted:
                     return True
         return False
@@ -90,107 +104,6 @@ class NFA:
                 for state_to_transition_to in states_to_transition_to:
                     transitions.append((transition_term, state_to_transition_to))
         return transitions
-
-# TEST 1 
-def d1(state_value, transition):
-    switch = {
-        ("A", "0"): ["B"],
-        ("A", "1"): ["C"],
-        ("B", "0"): ["B"],
-        ("B", "1"): ["D"],
-        ("C", "0"): ["B"],
-        ("C", "1"): ["C"],
-        ("D", "0"): ["B"],
-        ("D", "1"): ["E"],
-        ("E", "0"): ["B"],
-        ("E", "1"): ["C"],
-    }
-    if (state_value, transition) in switch.keys():
-        return switch[(state_value, transition)] 
-    return None
-n1 = NFA(["A", "B", "C", "D", "E"], ["0", "1"], d1, "A", ["E"])
-assert([('A', [("0", 'B'), ("1", 'C')], False), ('B', [("0", 'B'), ("1", 'D')], False), ('C', [("0", 'B'), ("1", 'C')], False), ('D', [("0", 'B'), ("1", 'E')], False), ('E', [("0", 'B'), ("1", 'C')], True)] == [state for state in map(lambda state: (state.value, state.transitions, state.accepted), n1.states)])
-
-#TEST 2
-def d2(state_value, transition):
-    switch = {
-        ("A", "0"): ["B"],
-        ("B", LAMBDA): ["F"],
-        ("C", "1"): ["D"],
-        ("D", LAMBDA): ["F"],
-        ("E", LAMBDA): ["C", "A"],
-        ("F", LAMBDA): ["E"],
-    }
-    if (state_value, transition) in switch.keys():
-        return switch[(state_value, transition)] 
-    return None
-n2 = NFA(["A", "B", "C", "D", "E", "F"], ["0", "1", LAMBDA], d2, "F", ["B", "D", "F", "E"])
-assert([('A', [("0", 'B')], False), ('B', [('', 'F')], True), ('C', [("1", 'D')], False), ('D', [('', 'F')], True), ('E', [('', 'C'), ('', 'A')], True), ('F', [('', 'E')], True)] == [state for state in map(lambda state: (state.value, state.transitions, state.accepted), n2.states)])
-n2Problem = NFAProblem("100001", n2)
-print(n2Problem.is_string_in_language(n2Problem.to_check, n2Problem.machine.start_state))
-
-#TEST 3
-def d3(state_value, transition):
-    switch = {
-        ("AE", "0"): ["BF"],
-        ("AE", "1"): ["DE"],
-        ("BF", "0"): ["CE"],
-        ("BF", "1"): ["CF"],
-        ("CE", "0"): ["DF"],
-        ("CE", "1"): ["DE"],
-        ("CF", "0"): ["DE"],
-        ("CF", "1"): ["DF"],
-        ("DE", "0"): ["DF"],
-        ("DE", "1"): ["DE"],
-        ("DF", "0"): ["DE"],
-        ("DF", "1"): ["DF"],
-    }
-    if (state_value, transition) in switch.keys():
-        return switch[(state_value, transition)] 
-    return None
-n3 = NFA(["AE", "BF", "CE", "CF", "DE", "DF"], ["0", "1"], d3, "AE", ["DE", "AE", "CE", "CF"])
-assert([('AE', [("0", 'BF'), ("1", 'DE')], True), ('BF', [("0", 'CE'), ("1", 'CF')], False), ('CE', [("0", 'DF'), ("1", 'DE')], True), ('CF', [("0", 'DE'), ("1", 'DF')], True), ('DE', [("0", 'DF'), ("1", 'DE')], True), ('DF', [("0", 'DE'), ("1", 'DF')], False)] == [state for state in map(lambda state: (state.value, state.transitions, state.accepted), n3.states)])
-
-#TEST 4 WEIRD CASE TO LOOK FOR LATER WHERE CAN TAKE MULTIPLE PATHS 
-# WITH THE SAME TRANSITION 
-def d4(state_value, transition):
-    switch = {
-        ("A", LAMBDA): ["B", "F"],
-        ("B", LAMBDA): ["C", "I"],
-        ("C", "1"): ["D"],
-        ("D", "0"): ["E"],
-        ("E", LAMBDA): ["B", "I"],
-        ("F", "0"): ["G"],
-        ("G", "1"): ["H"],
-        ("H", LAMBDA): ["I"],
-        ("I", "0"): ["J"]
-    }
-    if (state_value, transition) in switch.keys():
-        return switch[(state_value, transition)] 
-    return None
-n4 = NFA(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"], ["0", "1", LAMBDA], d4, "A", ["B", "E", "H", "I", "J"])
-assert([('A', [('', 'B'), ('', 'F')], False), ('B', [('', 'C'), ('', 'I')], True), ('C', [("1", 'D')], False), ('D', [("0", 'E')], False), ('E', [('', 'B'), ('', 'I')], True), ('F', [("0", 'G')], False), ('G', [("1", 'H')], False), ('H', [('', 'I')], True), ('I', [("0", 'J')], True), ('J', [], True)]
- == [state for state in map(lambda state: (state.value, state.transitions, state.accepted), n4.states)])
-
- # TEST 5 WHICH IS ALSO A WEIRD EDGE CASE TO WATCH OUT FOR LATER 
-def d5(state_value, transition):
-    switch = {
-        ("A", "0"): ["A", "B"],
-        ("A", "1"): ["A"],
-        ("B", "0"): ["C"],
-        ("B", "1"): ["C"]
-    }
-    if (state_value, transition) in switch.keys():
-        return switch[(state_value, transition)] 
-    return None
-n5 = NFA(["A", "B", "C"], ["0", "1"], d5, "A", ["C"])
-assert([('A', [('0', 'A'), ('0', 'B'), ('1', 'A')], False), ('B', [('0', 'C'), ('1', 'C')], False), ('C', [], True)] == [state for state in map(lambda state: (state.value, state.transitions, state.accepted), n5.states)])
-n5Problem = NFAProblem("10", n5)
-n5Problem2 = NFAProblem("10101", n5)
-print(n5Problem.is_string_in_language(n5Problem.to_check, n5Problem.machine.start_state))
-print(n5Problem2.is_string_in_language(n5Problem2.to_check, n5Problem.machine.start_state))
-# n4Problem = NFAProblem("1010", n4)
-# n4Problem.is_string_in_language(n4Problem.to_check, n4Problem.machine.states[0])
 
 def main():
     stdin = NFAStdin(sys.argv[1])
